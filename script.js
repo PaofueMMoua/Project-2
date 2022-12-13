15761812dd690a3315a37ad61a385f3709024a79
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2022-12-13 13:17:18 -0800

    edited Css

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-$(document).ready(function(){
-	$('dropdown').click(function(){
-		$(this).toggleClass('open');
-	});
-});
+// function myFunction() {
+// 	document.getElementById("myDropdown").classList.toggle("show");
+// }
+  
+// window.onclick = function(event) {
+// 	if (!event.target.matches('.dropbtn')) {
+// 	  var dropdowns = document.getElementsByClassName("dropdown-content");
+// 	  var i;
+// 	  for (i = 0; i < dropdowns.length; i++) {
+// 		var openDropdown = dropdowns[i];
+// 		if (openDropdown.classList.contains('show')) {
+// 		  openDropdown.classList.remove('show');
+// 		}
+// 	  }
+// 	}
+// }
